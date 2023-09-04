8389ed03ac8d3d8bd1502d44bf94c40f1eddd262
Anastasia Astakhova <69476057+K0k0drill0@users.noreply.github.com>
2023-09-04 23:17:32 +0300

    Create student.h

---
+++ cp6/student.h
@@ -0,0 +1,19 @@
+#ifndef _student_h_
+#define _student_h_
+
+typedef struct {
+  char second_name[100];
+  char initials[100];
+  char gender[100];
+  int group_num;
+  int architecture;
+  int VARKT;
+  int discra;
+  int history;
+  int LAAG;
+  int mat_analisys;
+  int fund_e;
+  int fund_kp;
+} student;
+
+#endif
